4643a43285a924af7b81d20ca2c4aaacf2624961
Sophiaa-c <yousophiachuks211@gmail.com>
2022-09-12 18:16:03 -0700

    commit for task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * Description:main -the main entyr point
+ *
+ *
+ *
+ * Return: Always return 0 (success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{	
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}	
