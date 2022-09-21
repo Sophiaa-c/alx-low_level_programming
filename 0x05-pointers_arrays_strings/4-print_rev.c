c5e28f9b4e970e6a0b2fab633329d0d6479ce581
Sophiaa-c <yousophiachuks211@gmail.com>
2022-09-20 18:55:27 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - This function prints a string of text in the reverse order
+ * @s: the param (string) to be printed)
+ * Return: nothing
+ */
+void print_rev(char *c)
+{
+	int i, n;
+
+	while (s[n] != '\0')
+	{
+		n++;
+	}
+	for (i = n - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
