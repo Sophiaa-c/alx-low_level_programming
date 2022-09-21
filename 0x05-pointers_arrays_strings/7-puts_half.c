5a9101efa9163fad70ca4de616dd2da02f430758
Sophiaa-c <yousophiachuks211@gmail.com>
2022-09-20 19:27:59 -0700

    fixed the prvious and crteated a new

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts_half - puts half
+ * @str: str parameter (pointer)
+ */
+void puts_half(char *str)
+{
+	int x, y, i;
+
+	x = strlen(str);
+	if (x % 2 != 0)
+		y = x - 1 / 2;
+	else
+		y = x / 2;
+	for (i = y; i < x; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
