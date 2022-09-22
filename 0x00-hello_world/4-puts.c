115c63194b9854f2cd06e1a1b14ec982fe7bfa96
Sophiaa-c <yousophiachuks211@gmail.com>
2022-09-22 08:10:31 -0700

    fixed

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the main entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char x[100] = "\"Programming is like building a multilingual puzzle";
+
+	puts(x);
+	return (0);
+}
+
