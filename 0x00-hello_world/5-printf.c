8a3dde4475bd416e5b28fb52932b0e12715535d6
Sophiaa-c <yousophiachuks211@gmail.com>
2022-09-22 08:26:30 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the main entry point 
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char x[100] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", x);
+	return (0);
+}
