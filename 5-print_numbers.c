b7c97bb38950eca713e7bea75e85c8572c45bb81
Sophiaa-c <yousophiachuks211@gmail.com>
2022-09-22 20:16:32 -0700

    task 6

---
+++ 5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * Description:main- The entry point
+ * Initialization: The variables aare initialised here
+ *
+ * Return: Always return zero (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+	putchar('\n');
+	return (0);
+}
